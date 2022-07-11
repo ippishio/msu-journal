5b145374fbf01078b8259b071fe1261012f9ecfb
Capybara <yskiwolf01@rambler.ru>
2022-07-11 19:57:12 +0300

    init commt

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Head from './components/head'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Head/>
+    </div>
+   
+  )
+}
+
+export default App;
