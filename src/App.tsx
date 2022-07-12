7fc33e6c08ee3f75d067b33baa7ace41fa075812
Capybara <yskiwolf01@rambler.ru>
2022-07-12 20:22:39 +0300

    egor

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import Head from './components/head'
 import './App.css';
+import Text from './components/body_t';
 
+import Sw from './components/swiper';
 function App() {
   return (
     <div>
       <Head/>
+      <Text/>
+      <Sw/>
     </div>
    
   )
